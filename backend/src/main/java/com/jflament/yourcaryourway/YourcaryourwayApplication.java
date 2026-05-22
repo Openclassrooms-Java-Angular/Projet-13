6fe1f0ff4b25138fd9455fa3e92b61518623f9f4
Jocelyn Flament <jocelyn.flament@gmail.com>
2026-05-22 20:06:38 +0200

    Mise en place du back-end

---
+++ backend/src/main/java/com/jflament/yourcaryourway/YourcaryourwayApplication.java
@@ -0,0 +1,13 @@
+package com.jflament.yourcaryourway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourcaryourwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourcaryourwayApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/jflament/yourcaryourway/YourcaryourwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jflament.yourcaryourway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourcaryourwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
